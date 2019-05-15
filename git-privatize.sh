fb642cf72ba9a05dd4c7e893c257572b1ec804e7
Ethan Nam <ethan.s.nam@gmail.com>
2019-05-15 05:27:18 -0400

    Output bold formatting and repo count

--- git-privatize.sh
+++ git-privatize.sh
@@ -14,20 +14,43 @@ done
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
+total_repos=$(find $parent_path -type d -maxdepth 1 | wc -l)
+((total_repos--))
+
+count=1
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+
 for file in *; do
   if [ -d "$file" ]; then
     cd "$file"
-    printf "\nDeleting remote repo $file on GitHub"
+
+    message="Deleting remote repo $file on GitHub..."
+    printf "\n\n${bold}$message${normal}\n"
     hub delete -y $file
-    printf "\nCreating new private GitHub repo with name $file"
+
+    message="Creating new private GitHub repo with name $file..."
+    printf "\n\n${bold}$message${normal}\n"
     hub create -p $file
-    printf "\nSetting this local repository's remote URL to the recently created private repo's URL"
+
+    message="Setting this local repository's remote URL to the recently created private repo's URL..."
+    printf "\n\n${bold}$message${normal}\n"
     git remote set-url origin git@github.com:$username/$file.git
-    printf "\nPushing to GitHub"
+    printf "Set new remote url.\n"
+
+    message="Pushing to GitHub..."
+    printf "\n\n${bold}$message${normal}\n"
     git push
-    printf "\nSuccess!"
+
+    message="Successfully privatized repo $count of $total_repos"
+    printf "\n\n${bold}$message${normal}\n"
+
+    ((count++))
+
     cd ..
   fi
 done
 
-printf "\nAll done!"
+message="All done 🤗 🤗 🤗"
+printf "\n\n${bold}$message${normal}\n"
